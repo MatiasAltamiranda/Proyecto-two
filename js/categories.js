f99bb1984a8371605b1f69f60a370db375f9826c
Matias <matias_lma94@hotmail.com>
2022-03-30 20:33:08 -0300

    update categories.js | index.js | index.html

--- js/categories.js
+++ js/categories.js
@@ -1,53 +1,28 @@
-
+function createCategories(){
 
 async function getCategorie(){
     const apiURL ='http://localhost:4000/movies';
     const response = await fetch(`${apiURL}`);
     const data = await response.json();
-    createCardAnimated(data);
-    createCardAction(data, categoryAction, 'accion');
-    createCardComedy(data);
-    createCardFeatured(data);
+    createCards(data,categorieAnimated, 'animada');
+    createCards(data, categoryAction, 'accion');
+    createCards(data, categorieComedy,'comedia');
+    createCardsFeatured(data);
 }
 
- getCategorie()
+getCategorie()
 
-function createCardAnimated(data){
-    const categorieAnimated = document.querySelector('#categorieAnimated');
-    data = data.filter(e => e.category == "animada")
-    data.forEach(e => {
-        const divCard = document.createElement('div');
-        divCard.classList= 'col-6 col-sm-6 col-md-4 col-lg-3';
-        divCard.innerHTML=`<a href="#"><img class="img-fluid p-3" src="${e.image}" alt="Image ${e.name}">`
-        categorieAnimated.append(divCard)
-    })
-    ;
-}
+const categorieAnimated = document.querySelector('#categorieAnimated');
 const categoryAction = document.querySelector('#categorieAction');
+const categorieComedy = document.querySelector('#categorieComedy');
 
-function createCardAction(data, container,category){
-    data = data.filter(e => e.category == category)
-    data.forEach(e => {
-        const divCard = document.createElement('div');
-        divCard.classList= 'col-6 col-sm-6 col-md-4 col-lg-3';
-        divCard.innerHTML=`<a href="#"><img class="img-fluid p-3" src="${e.image}" alt="Image ${e.name}">`
-        container.append(divCard)
-    });
+function createCards(data, container,category){
+    data = data.filter(e => e.category === category)
+    createCard(data,container);
 }
 
 
-function createCardComedy(data){
-    const categorieComedy = document.querySelector('#categorieComedy');
-    data = data.filter(e => e.category == "comedia")
-    data.forEach(e => {
-        const divCard = document.createElement('div');
-        divCard.classList= 'col-6 col-sm-6 col-md-4 col-lg-3';
-        divCard.innerHTML=`<a href="#"><img class="img-fluid p-3" src="${e.image}" alt="Image ${e.name}">`
-        categorieComedy.append(divCard)
-    });
-}
-
-function createCardFeatured(data){
+function createCardsFeatured(data){
     const categorieFeatured = document.querySelector('#categorieFeatured');
     data = data.filter(e => e.featured == true)
     data.forEach(e => {
@@ -58,4 +33,15 @@ function createCardFeatured(data){
     });
 }
 
-export default getCategorie;
+function createCard(data,container){
+    data.forEach(e => {
+        const divCard = document.createElement('div');
+        divCard.classList= 'col-6 col-sm-6 col-md-4 col-lg-3';
+        divCard.innerHTML=`<a href="#"><img class="img-fluid p-3" src="${e.image}" alt="Image ${e.name}">`
+        container.append(divCard)
+    });
+}
+
+}
+
+export default createCategories;
