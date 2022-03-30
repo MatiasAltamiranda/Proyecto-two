817911a2afdecefc0355fc73e8888f1468c75f91
Matias <matias_lma94@hotmail.com>
2022-03-30 20:40:40 -0300

    update categories.js | index.js | index.html

--- js/categories.js
+++ js/categories.js
@@ -7,7 +7,7 @@ async function getCategorie(){
     createCards(data,categorieAnimated, 'animada');
     createCards(data, categoryAction, 'accion');
     createCards(data, categorieComedy,'comedia');
-    createCardsFeatured(data);
+    createCardsFeatured(data, categorieFeatured);
 }
 
 getCategorie()
@@ -15,22 +15,16 @@ getCategorie()
 const categorieAnimated = document.querySelector('#categorieAnimated');
 const categoryAction = document.querySelector('#categorieAction');
 const categorieComedy = document.querySelector('#categorieComedy');
+const categorieFeatured = document.querySelector('#categorieFeatured');
 
 function createCards(data, container,category){
     data = data.filter(e => e.category === category)
     createCard(data,container);
 }
 
-
-function createCardsFeatured(data){
-    const categorieFeatured = document.querySelector('#categorieFeatured');
+function createCardsFeatured(data,container){
     data = data.filter(e => e.featured == true)
-    data.forEach(e => {
-        const divCard = document.createElement('div');
-        divCard.classList= 'col-6 col-sm-6 col-md-4 col-lg-3';
-        divCard.innerHTML=`<a href="#"><img class="img-fluid p-3" src="${e.image}" alt="Image ${e.name}">`
-        categorieFeatured.append(divCard)
-    });
+    createCard(data,container);
 }
 
 function createCard(data,container){
