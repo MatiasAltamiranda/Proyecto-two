8a79ff508aa95e55e5852eb06ee2909031f134ff
Federico <federico_corby@hotmail.com>
2022-04-15 20:33:55 -0300

    rediseño navbar

--- ui/navBar.js
+++ ui/navBar.js
@@ -1,8 +1,11 @@
 export function createNavBar(){
     const header = document.querySelector('#header')
     const divHeader = document.createElement('div')
-    divHeader.className = "container bg-black"
+    divHeader.className = "bg-black"
     divHeader.innerHTML = `<ul class="nav justify-content-end">
+    <div>
+    <a href=/index.html> <img src=/img/logo.png width=35px> </a>
+    </div>
     <li class="nav-item">
       <a class="nav-link disabled" id="welcomeUser"></a>
     </li>
@@ -19,34 +22,46 @@ export function createNavBar(){
         </a>
         <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
           <li>
-            <a class="dropdown-item" href="#">Acerca de losotro</a>
+            <a class="dropdown-item" href="/about-us.html">Acerca de Nosotro</a>
           </li>
-          <li><a class="dropdown-item" href="#">Another action</a></li>
-          <li>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </li>
+          <li><a class="dropdown-item" href="#">contactanos!</a></li>
         </ul>
       </div>
     </li>
     <li class="nav-item">
-      <a class="nav-link">Logueate!</a>
+      <a id="aCloseSession" href=/login.html>Logueate!</a>
     </li>
   </ul>`;
   header.append(divHeader)
 
+  // MENSAJE DE BIENVENIDA
   function welcomeUser (){
     const loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
     const messageWelcomeUser = document.querySelector('#welcomeUser');
+    const aCloseSession = document.querySelector('#aCloseSession');
     if(loggedUser){
         const welcomeMessage = document.createElement('p')
-        welcomeMessage.textContent = `Bienvenido ${loggedUser.name}`;
+        welcomeMessage.textContent = `Bienvenido ${loggedUser.name}!`;
         messageWelcomeUser.append(welcomeMessage);
+        aCloseSession.textContent = 'Cerrar sesion'
     }
 }
 welcomeUser();
 
 
 
+
+// DESLOGUEO
+const CloseSession = document.querySelector('#aCloseSession');
+
+CloseSession.addEventListener('click', () => {
+  localStorage.removeItem('loggedUser');
+  window.location.href = '/index.html'
+})
+
+
+
+// VALIDAR INPUT 
 function validateInput(){
     const parentDiv = this.parentElement;
     if(this.type === 'email'){
