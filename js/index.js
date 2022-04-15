a1a1662b96c169498b3e943bbdddbe844d20e8d2
Federico <federico_corby@hotmail.com>
2022-04-14 23:00:26 -0300

    create login and register

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
+const closeSession = document.querySelector('#closse-sesion');
+let isLoggedUser = JSON.parse(localStorage.getItem('loggedUser'));
+
+closeSession.addEventListener('click', () => {
+    localStorage.removeItem('loggedUser');
+    window.location.href = '/login.html'
+})
 
 
+
+
+if(!isLoggedUser) {
+    window.location.href = '/login.html'
+}
